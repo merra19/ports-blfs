1212c58d5c6ad5002dba0bb9aac7f4ad3fd533eb
merra19 <15358838+merra19@users.noreply.github.com>
2025-06-16 21:00:04 +0200

    fix polybar script launch

--- openbox/openbox-meta/launch.sh
+++ openbox/openbox-meta/launch.sh
@@ -6,11 +6,4 @@
 killall -q polybar
 
 wait
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    echo $m
-    MONITOR=$m polybar --reload bar &
-  done
-else
-  polybar --reload bar &
-fi
+polybar --reload bar &
