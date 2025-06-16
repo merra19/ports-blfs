5a44a9ba7ef3d28aaa799f0c29400df3bdc5c9c0
merra19 <15358838+merra19@users.noreply.github.com>
2025-06-16 18:50:34 +0200

    add slop

--- openbox/openbox-meta/launch.sh
+++ openbox/openbox-meta/launch.sh
@@ -5,8 +5,12 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log 
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+wait
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
